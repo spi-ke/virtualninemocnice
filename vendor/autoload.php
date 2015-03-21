57859c1baf0c2073cac406fd0d3cc56acdee72c8
Ales Zavadsky <ales.zavadsky@lmc.eu>
2015-03-22 00:29:45 +0100

    init commit

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit148b12f2685cf4fdadc00c36c9cb4797::getLoader();
+return ComposerAutoloaderInite12ff213bcaa9fe0d09d1950956ca243::getLoader();
